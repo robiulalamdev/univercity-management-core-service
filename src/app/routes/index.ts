7dc8637931872f53726a07c3ee9150326466a39d
robiulalam76 <robiulalam762400@gmail.com>
2023-08-20 12:52:16 +0600

    project update and prisma added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,12 @@
 import express from 'express';
-
 const router = express.Router();
 
-const moduleRoutes = [
-  // ... routes
-  {
-    path: "",
-    routes: ""
-  }
-];
+// const moduleRoutes = [
+//   {
+//     path: '/users',
+//     route: UserRoutes,
+//   },
+// ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+// moduleRoutes.forEach(route => router.use(route.path, route.route));
 export default router;
