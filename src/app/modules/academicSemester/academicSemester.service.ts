91552af211c6951b6288ebd96c25f26ca939ccbc
robiulalam76 <robiulalam762400@gmail.com>
2023-08-20 22:33:49 +0600

    academic semester pagination done

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,12 @@
-import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
+import {
+  AcademicSemesterFilterAbleFileds,
+  AcademicSemesterSearchAbleFields,
+} from './academicSemester.constant';
+import { IAcademicSemesterFilter } from './academicSemester.interface';
 
 const prisma = new PrismaClient();
 
@@ -15,14 +20,42 @@ const insertIntoDB = async (
 };
 
 const getDataFromDB = async (
-  filters: any,
+  filters: IAcademicSemesterFilter,
   options: IPaginationOptions
 ): Promise<IGenericResponse<AcademicSemester[]>> => {
   const { limit, skip, page } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: AcademicSemesterSearchAbleFields.map(filed => ({
+        [filed]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: AcademicSemesterFilterAbleFileds.map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
 
   const result = await prisma.academicSemester.findMany({
     skip,
     take: limit,
+    where: whereConditions,
   });
 
   const total = await prisma.academicSemester.count();
